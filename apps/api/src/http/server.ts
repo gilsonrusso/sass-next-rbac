cb23e5f4389f3c7d829e054f9048201646c85a77
Gilson <gilsonrusso@outlook.com>
2025-12-02 21:48:31 -0400

    Add swagger docs

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,12 +1,15 @@
 import { fastify } from 'fastify'
 import fastifyCors from '@fastify/cors'
 import {
+  jsonSchemaTransform,
   //   jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 import { createAccount } from './routes/auth/create-account'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUI from '@fastify/swagger-ui'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -15,7 +18,22 @@ app.register(fastifyCors, {
 })
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
-// app.addSchemaTransform(jsonSchemaTransform)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Next Saas API',
+      description: 'Full Stack Saas app with muilt-tenant & RBAC',
+      version: '1.0.0',
+    },
+    servers: [],
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUI, {
+  routePrefix: '/docs',
+})
 
 app.get('/health', (_, replay) => {
   return replay.status(200).send({ message: 'API OK' })
