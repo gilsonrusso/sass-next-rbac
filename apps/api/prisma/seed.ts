4899657d4c2514a4792a73319554d4521a605e47
Gilson <gilsonrusso@outlook.com>
2025-12-02 21:10:23 -0400

    Create seed with faker

---
+++ apps/api/prisma/seed.ts
@@ -0,0 +1,150 @@
+import { PrismaClient } from '../src/generated/prisma/client.ts'
+import { env } from '../src/env/index.ts'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { faker } from '@faker-js/faker'
+import { hash } from 'bcryptjs'
+import { fa } from 'zod/v4/locales'
+
+const connectionString = `${env.DATABASE_URL}`
+
+const adapter = new PrismaPg({ connectionString })
+const prismaClient = new PrismaClient({
+  adapter,
+})
+
+async function seed() {
+  await prismaClient.organization.deleteMany()
+  await prismaClient.user.deleteMany()
+
+  const users = await Promise.all([
+    prismaClient.user.create({
+      data: {
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+        passwordHash: await hash('password123', 1),
+      },
+    }),
+    prismaClient.user.create({
+      data: {
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+        passwordHash: await hash('password123', 1),
+      },
+    }),
+    prismaClient.user.create({
+      data: {
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+        passwordHash: await hash('password123', 1),
+      },
+    }),
+  ])
+
+  const [firstUser, secondUser, thirdUser] = users
+
+  await prismaClient.organization.create({
+    data: {
+      name: 'Acme Corp (ADMIN)',
+      domain: 'acme.com',
+      slug: faker.helpers.slugify(faker.company.name() + '-admin'),
+      avatarUrl: faker.image.url(),
+      shouldAttachUserByDomain: true,
+      ownerId: firstUser.id,
+      projects: {
+        createMany: {
+          data: Array.from({ length: 3 }).map(() => ({
+            name: faker.commerce.productName(),
+            description: faker.commerce.productDescription(),
+            slug: faker.helpers.slugify(
+              faker.commerce.productName() + '-' + faker.string.uuid()
+            ),
+            avatarUrl: faker.image.url(),
+            ownerId: faker.helpers.arrayElement([
+              firstUser.id,
+              secondUser.id,
+              thirdUser.id,
+            ]),
+          })),
+        },
+      },
+      members: {
+        create: [
+          { userId: firstUser.id, role: 'ADMIN' },
+          { userId: secondUser.id, role: 'MEMBER' },
+          { userId: thirdUser.id, role: 'MEMBER' },
+        ],
+      },
+    },
+  })
+
+  await prismaClient.organization.create({
+    data: {
+      name: 'Acme Corp (MEMBER)',
+      slug: faker.helpers.slugify(faker.company.name() + '-member'),
+      avatarUrl: faker.image.url(),
+      ownerId: firstUser.id,
+      projects: {
+        createMany: {
+          data: Array.from({ length: 3 }).map(() => ({
+            name: faker.commerce.productName(),
+            description: faker.commerce.productDescription(),
+            slug: faker.helpers.slugify(
+              faker.commerce.productName() + '-' + faker.string.uuid()
+            ),
+            avatarUrl: faker.image.url(),
+            ownerId: faker.helpers.arrayElement([
+              firstUser.id,
+              secondUser.id,
+              thirdUser.id,
+            ]),
+          })),
+        },
+      },
+      members: {
+        create: [
+          { userId: firstUser.id, role: 'MEMBER' },
+          { userId: secondUser.id, role: 'ADMIN' },
+          { userId: thirdUser.id, role: 'MEMBER' },
+        ],
+      },
+    },
+  })
+
+  await prismaClient.organization.create({
+    data: {
+      name: 'Acme Corp (Billing)',
+      slug: faker.helpers.slugify(faker.company.name() + '-billing'),
+      avatarUrl: faker.image.url(),
+      ownerId: firstUser.id,
+      projects: {
+        createMany: {
+          data: Array.from({ length: 3 }).map(() => ({
+            name: faker.commerce.productName(),
+            description: faker.commerce.productDescription(),
+            slug: faker.helpers.slugify(
+              faker.commerce.productName() + '-' + faker.string.uuid()
+            ),
+            avatarUrl: faker.image.url(),
+            ownerId: faker.helpers.arrayElement([
+              firstUser.id,
+              secondUser.id,
+              thirdUser.id,
+            ]),
+          })),
+        },
+      },
+      members: {
+        create: [
+          { userId: firstUser.id, role: 'BILLING' },
+          { userId: secondUser.id, role: 'ADMIN' },
+          { userId: thirdUser.id, role: 'MEMBER' },
+        ],
+      },
+    },
+  })
+}
+
+seed().then(async () => {
+  console.log('Seeding finished.')
+  await prismaClient.$disconnect()
+})
